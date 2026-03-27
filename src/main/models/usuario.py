bc70f2c71e7a62ed00b374dd70f1c3e9c2c721cc
Joaquimfl8 <160670206+Joaquimfl8@users.noreply.github.com>
2026-03-27 10:43:04 -0300

    Update usuario.py

--- src/main/models/usuario.py
+++ src/main/models/usuario.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 
 class Usuario(BaseModel):
-    id: int
-    nome: str
-    gmail: str
+    id: int = Field(...)
+    nome: str = Field(..., min_length = 3)
+    gmail: EmailStr
     telefone: str
     nivelPerm: int
     
