71aeb04f3aaacc4d77ca5e168f3c9fb86f2b4bb3
Joaquimfl8 <160670206+Joaquimfl8@users.noreply.github.com>
2026-03-27 11:15:43 -0300

    Update usuario.py

--- src/main/models/usuario.py
+++ src/main/models/usuario.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, EmailStr, Field
 
 class Usuario(BaseModel):
     id: int = Field(..., gt = 0)
-    nome: str = Field(..., min_length = 3)
+    nome: str = Field(..., min_length=3, max_length=25)
     gmail: EmailStr
     telefone: str = Field(min_length=11, max_length=11)
     nivelPerm: int = Field(default=1, gt=0, le=5)
